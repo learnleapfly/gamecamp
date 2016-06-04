57b8de4470f4d09d950d4d84e1a831f838d032af
Dorian Pula <dorian.pula@amber-penguin-software.ca>
2016-06-04 12:23:40 -0700

    Add grid layout with coordinates for map.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from kivy.app import App
+from kivy import properties
+from kivy.uix import button
 from kivy.uix.floatlayout import FloatLayout
 
 
 class StrategyGame(FloatLayout):
-    pass
+    main_map = properties.ObjectProperty(None)
+    map_rows = properties.NumericProperty(0)
+    map_cols = properties.NumericProperty(0)
+
+    def __init__(self, **kwargs):
+        super(StrategyGame, self).__init__(**kwargs)
+
+        number_of_regions = self.map_rows * self.map_cols
+        for region in xrange(0, number_of_regions):
+            row = region / self.map_cols
+            col = region % self.map_cols
+            self.main_map.add_widget(button.Button(text='({}, {})'.format(row, col)))
 
 
 class StrategyGameApp(App):
